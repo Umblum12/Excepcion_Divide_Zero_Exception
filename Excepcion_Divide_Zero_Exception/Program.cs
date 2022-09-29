b1d608919787aea079747407ef98c2344951fdb3
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:42:36 -0500

    Initial commit

---
+++ Excepcion_Divide_Zero_Exception/Program.cs
@@ -0,0 +1,16 @@
+﻿using Excepcion_Divide_Zero_Exception;
+
+namespace Excepcion_Divide_Zero_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorUno = 10, valorDos = 0, resultado;
+
+            resultado = valorUno / valorDos;
+            
+            Console.WriteLine(resultado);
+        }
+    }
+}
